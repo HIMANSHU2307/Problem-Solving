c07e307f48fead09d18af536f40871f5c0e9a53f
Himanshu Sharma <34489523+HIMANSHU2307@users.noreply.github.com>
2020-09-07 14:32:13 +0530

    Update multiplePointer.js

--- multiplePointer.js
+++ multiplePointer.js
@@ -25,3 +25,24 @@ console.log(sumZero(arr));
 
 // Time Complexity O(n)
 // Space O(1)
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// REFACTORING: To count the unique values from a sorted array
+
+const arr =[1, 1, 1, 1, 1, 2, 3]; // 3(3 unique numbers)
+
+
+function countUniqueValues(arr) {
+  var i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i+1;
+}
+
+console.log(countUniqueValues(arr));
